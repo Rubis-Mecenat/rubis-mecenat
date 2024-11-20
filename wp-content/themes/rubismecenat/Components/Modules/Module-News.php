593dd723d2538a8d0a43b585c4244811e4470b14
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-11-20 15:31:46 +0100

    style: set news module layout

--- wp-content/themes/rubismecenat/Components/Modules/Module-News.php
+++ wp-content/themes/rubismecenat/Components/Modules/Module-News.php
@@ -4,17 +4,17 @@
 ?>
 
 
-<section class="mod_carousel">
+<section class="mod_news">
 
-    <header class="mod_header wrapper">
-        <h2><?php echo $title; ?></h2>
+    <header class="mod_header">
+        <h2 class="txt-center mt-xxl"><?php echo $title; ?></h2>
     </header>
 
-    <div class="mod_publication wrapper">
+    <div class="mod_publication">
         <?php if( $publication ): ?>
-            <article class="grid">
+            <article class="grid gap-xl">
                 <div class="s-6col">
-                    <a href="<?php echo get_permalink($publication->ID); ?>"><?php  echo $publication->post_title; ?></a>
+                    <a class="h2" href="<?php echo get_permalink($publication->ID); ?>"><?php  echo $publication->post_title; ?></a>
                 </div>
                 <div class="s-6col">
                     <?php echo get_the_post_thumbnail( $publication->ID, 'full' ); ?>
