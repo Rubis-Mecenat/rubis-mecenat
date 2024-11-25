534fff2334a4c5707636e5b11bb47f876a498aef
Thomas <hello@thomasflorentin.net>
2024-11-25 10:51:14 +0100

    css: Polish

--- wp-content/themes/rubismecenat/Components/Blocks/Block-Project.php
+++ wp-content/themes/rubismecenat/Components/Blocks/Block-Project.php
@@ -8,10 +8,10 @@
         <?php endif; ?>
 
         <div class="">
-            <h3><?php the_title(); ?></h3>
-            <p><?php the_field('project_subtitle'); ?></p>
-            <p><?php the_field('project_place'); ?></p>
-            <p><?php the_field('project_artiste'); ?></p>
+            <h3 class="h3"><?php the_title(); ?></h3>
+            <p class="h3 -other"><?php the_field('project_subtitle'); ?></p>
+            <p class="h3 -other"><?php the_field('project_place'); ?></p>
+            <p class="-light"><?php the_field('project_artiste'); ?></p>
         </div>
     </a>
 </article>
