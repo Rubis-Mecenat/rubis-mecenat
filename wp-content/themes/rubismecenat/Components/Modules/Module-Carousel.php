289046a777a357c3ea1844c1d8f64652bbf29758
Thomas <hello@thomasflorentin.net>
2024-11-28 14:24:10 +0100

    Update Module-Carousel.php

--- wp-content/themes/rubismecenat/Components/Modules/Module-Carousel.php
+++ wp-content/themes/rubismecenat/Components/Modules/Module-Carousel.php
@@ -27,7 +27,7 @@
 
             <div class="swiper-wrapper">
 
-                <?php foreach ($posts as $post) : setup_postdata($post); var_dump( $post )?>
+                <?php foreach ($posts as $post) : setup_postdata($post); var_dump( get_post_type() )?>
                  
                     <div class="swiper-slide">
                         <?php get_template_part('Components/Blocks/Block', get_post_type()); ?>
