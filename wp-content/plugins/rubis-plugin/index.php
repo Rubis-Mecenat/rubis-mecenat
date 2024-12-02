bfa60f737dc00f326f894b56f78a3e36be5156b7
Thomas <hello@thomasflorentin.net>
2024-12-02 16:16:12 +0100

    Update index.php

--- wp-content/plugins/rubis-plugin/index.php
+++ wp-content/plugins/rubis-plugin/index.php
@@ -20,4 +20,3 @@ require_once(RUBIS_DIR.'/cpt/cpt-artist.php');
 
 require_once(RUBIS_DIR.'/taxonomies/tax-editions.php');
 require_once(RUBIS_DIR.'/taxonomies/tax-videos.php');
-require_once(RUBIS_DIR.'/taxonomies/tax-projects.php');
