61725f2879a649d5b343f53227b37f812beb1456
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-12-13 11:16:14 +0100

    style: fix horizontal carousel scrollbar height & vertical text position

--- wp-content/themes/rubismecenat/Components/Templates/Template-transmettre.php
+++ wp-content/themes/rubismecenat/Components/Templates/Template-transmettre.php
@@ -7,7 +7,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="wrapper -small gap-0 mb-0">
-        <div class="breadcrumb_container s-12col">
+        <div class="breadcrumb_container horizontal-slide s-12col">
             <?php get_template_part('Components/Modules/Module', "Breadcrumbs"); ?>
         </div>
 	</header>
