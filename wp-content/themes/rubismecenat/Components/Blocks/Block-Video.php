86d3ad7a3ab65f4d8bc821ed4998c75837812b56
Thomas <hello@thomasflorentin.net>
2025-01-23 14:02:09 +0100

    blocks: Polish placeholder

--- wp-content/themes/rubismecenat/Components/Blocks/Block-Video.php
+++ wp-content/themes/rubismecenat/Components/Blocks/Block-Video.php
@@ -12,14 +12,17 @@
         data-type="<?php echo get_post_type(); ?>"
         class="js-load-modal -block">
 
-        <?php if (has_post_thumbnail()) : ?>
-            <div class="block-video__image mb-m">
+        <div class="block-video__image mb-m">
+            <?php if (has_post_thumbnail()) : ?>
                 <?php the_post_thumbnail('theme_small'); ?>
-                <span class="overlay flex -center">
-                    <?php get_template_part('Components/Svgs/Svg', 'Plus'); ?>
-                </span>
-            </div>
-        <?php endif; ?>
+            <?php else: ?>
+                <div class="block_placeholder"></div>
+            <?php endif; ?>
+
+            <span class="overlay flex -center">
+                <?php get_template_part('Components/Svgs/Svg', 'Plus'); ?>
+            </span>
+        </div>
 
         <div class="">
             <h3 class="h3 -other mb-xxs"><?php the_title(); ?></h3>
