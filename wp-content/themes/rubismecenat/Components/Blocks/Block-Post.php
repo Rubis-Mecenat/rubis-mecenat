524849ef8c548fd972f28fd3369f366cd89381d2
Thomas <hello@thomasflorentin.net>
2025-01-30 14:58:54 +0100

    Cleaning

--- wp-content/themes/rubismecenat/Components/Blocks/Block-Post.php
+++ wp-content/themes/rubismecenat/Components/Blocks/Block-Post.php
@@ -20,9 +20,20 @@
 
         <div class="">
             <h3 class="h3 mb-xxs"><?php the_title(); ?></h3>
-            <p class="h3 -other mb-xs"><?php echo wp_kses_post( get_field('project_subtitle') );  ?></p>
-            <p class="body -big -light mb-xs"><?php the_field('project_place'); ?></p>
-            <p class="-light"><?php the_field('project_artiste'); ?></p>
+            
+            <?php if( get_field('project_subtitle') ) : ?>
+                <p class="h3 -other mb-xs"><?php echo wp_kses_post( get_field('project_subtitle') ); ?></p>
+            <?php endif; ?>
+
+            <?php if( get_field('project_place') ) : ?>
+                <p class="body -big -light mb-xs"><?php the_field('project_place'); ?></p>
+            <?php endif; ?>
+
+            <?php if( get_field('project_artiste') ) : ?>
+                <p class="-light"><?php the_field('project_artiste'); ?></p>
+            <?php endif; ?>
+
+
         </div>
     </a>
 </article>
