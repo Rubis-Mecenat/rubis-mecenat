837dccabc41fe3f4411fde377928904cfcbf7965
Thomas <hello@thomasflorentin.net>
2025-03-04 10:22:47 +0100

    script: Dequeue Jquery

--- wp-content/themes/rubismecenat/functions.php
+++ wp-content/themes/rubismecenat/functions.php
@@ -138,7 +138,8 @@ function rubismecenat_scripts() {
 	// DEQUEUE
 	wp_dequeue_style('sbi_styles');
 	wp_register_style('sbi_styles_custom', get_template_directory_uri() . '/assets/sbi_custom.css', array(), _S_VERSION );
-
+    wp_dequeue_script( 'jquery');
+    wp_deregister_script( 'jquery');
 
 }
 add_action( 'wp_enqueue_scripts', 'rubismecenat_scripts' );
