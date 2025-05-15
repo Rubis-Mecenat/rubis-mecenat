13c4973d16fa21de3136b644442c50ba3952fc64
Thomas <hello@thomasflorentin.net>
2025-05-15 16:24:44 +0200

    Update header.php

--- wp-content/themes/rubismecenat/header.php
+++ wp-content/themes/rubismecenat/header.php
@@ -21,6 +21,7 @@
 
 	<!-- TarteAuCitron Cookies -->
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/tarteaucitron/tarteaucitron.js"></script>
+
 	<script type="text/javascript">
 		tarteaucitron.init({
 		"privacyUrl": "", /* Privacy policy url */
@@ -69,21 +70,22 @@
 	$host = $_SERVER['HTTP_HOST']; 
 	if($host == "www.rubismecenat.fr" ) : ?>
 
-		<!-- Matomo -->
-		<script>
-		var _paq = window._paq = window._paq || [];
-		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		(function() {
-			var u="http://stats.rubismecenat.fr/";
-			_paq.push(['setTrackerUrl', u+'matomo.php']);
-			_paq.push(['setSiteId', '1']);
-			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-		})();
-		</script>
-		<!-- End Matomo Code -->
+			<!-- Matomo -->
+			<script>
+			var _paq = window._paq = window._paq || [];
+			/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function() {
+				var u="//stats.rubismecenat.fr/";
+				_paq.push(['setTrackerUrl', u+'matomo.php']);
+				_paq.push(['setSiteId', '1']);
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+				g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+			})();
+			</script>
+			<!-- End Matomo Code -->
+
 				
 	<?php endif; ?>
 
